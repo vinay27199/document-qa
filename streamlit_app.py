fc00e61c191e3a85e0dc6aa27bb9685ee9f8a577
Vinay Kumar Chandra <99154944+vinay27199@users.noreply.github.com>
2024-09-05 14:22:39 +0000

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,6 @@
 import streamlit as st
-from openai import OpenAI
-
-# Show title and description.
-st.title("MY Document Question Answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
-
-# Ask user for their OpenAI API key via `st.text_input`.
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
-    try:
-        # Create an OpenAI client and validate the API key.
-        client = OpenAI(api_key=openai_api_key)
-        # Make a simple API call to validate the key.
-        client.models.list()
-        st.success("API key is valid!", icon="✅")
-
-        # Let the user upload a file via `st.file_uploader`.
-        uploaded_file = st.file_uploader(
-            "Upload a document (.txt or .md)", type=("txt", "md")
-        )
-
-        # Ask the user for a question via `st.text_area`.
-        question = st.text_area(
-            "Now ask a question about the document!",
-            placeholder="Can you give me a short summary?",
-            disabled=not uploaded_file,
-        )
-
-        if uploaded_file and question:
-            # Process the uploaded file and question.
-            document = uploaded_file.read().decode()
-            messages = [
-                {
-                    "role": "user",
-                    "content": f"Here's a document: {document} \n\n---\n\n {question}",
-                }
-            ]
-
-            # Generate an answer using the OpenAI API.
-            stream = client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=messages,
-                stream=True,
-            )
-
-            # Stream the response to the app using `st.write_stream`.
-            st.write_stream(stream)
-
-    except Exception as e:
-        st.error("Invalid API key. Please enter a valid OpenAI API key.", icon="❌")
+Lab1 = st.Page("lab1.py",title='Lab1')
+Lab2 = st.Page("lab2.py",title='Lab2')
+pg=st.navigation([Lab1,Lab2])
+st.set_page_config(page_title='Lab Apps')
+pg.run()
